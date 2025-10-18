9bb9df32745017e496172706f5792900f2394c8e
perez-gerardo <gerardo.perez@tecsup.edu.pe>
2025-10-17 23:58:46 -0500

    Initial commit: React app with Bootstrap table for article management

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articulos: [
+        {
+          codigo: 1,
+          descripcion: 'coca-cola',
+          precio: 2.50
+        },
+        {
+          codigo: 2,
+          descripcion: 'inka-cola',
+          precio: 2.20
+        },
+        {
+          codigo: 3,
+          descripcion: 'fanta',
+          precio: 1.70
+        }
+      ]
+    };
+    this.borrar = this.borrar.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="container mt-4">
+        <h1 className="text-center mb-4">Gestión de Artículos</h1>
+        <div className="table-responsive">
+          <table className="table table-striped table-hover table-bordered">
+            <thead className="table-dark">
+              <tr>
+                <th>Código</th>
+                <th>Descripción</th>
+                <th>Precio</th>
+                <th>Acción</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.articulos.map(elemento => {
+                return (
+                  <tr key={elemento.codigo}>
+                    <td>{elemento.codigo}</td>
+                    <td>{elemento.descripcion}</td>
+                    <td>${elemento.precio}</td>
+                    <td>
+                      <button 
+                        className="btn btn-danger btn-sm"
+                        onClick={() => this.borrar(elemento.codigo)}
+                      >
+                        Borrar
+                      </button>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+
+  borrar(cod) {
+    var temp = this.state.articulos.filter((el) => el.codigo !== cod);
+    this.setState({
+      articulos: temp
+    });
+  }
+}
+
+export default App;
